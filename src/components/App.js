8186c7b78f342d0c0c5a88345bc5a4272613e27c
Mozart <peterkayk@gmail.com>
2023-03-05 08:38:01 +0300

    Adding semantic ui and app components

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React from 'react';
+import Header from '/Header';
+import AddContact from '/AddContact';
+import ContactList from './ContactList';
+
+
+function App() {
+
+  const contacts = [
+    {
+      id: "1",
+      name: "Peter",
+      email: "peterkayk@gmail.com"
+    },
+    {
+      id: "2",
+      name: "Mozart",
+      email: "mozart234@gmail.com"
+    }
+  ]
+
+  return (
+    <div className='ui container'>
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CardContact = () => {
+    return (
+        <div className="item">
+            <div className="content">
+                <div className="header">
+                    {contact.name}
+                </div>
+                <div>
+                    {contact.email}
+                </div>
+            </div>
+            <i className="trash alternate outline icon"></i>
+        </div>
+    );
+}
+
+export default contactCard;

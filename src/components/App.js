a12b19ee63bd16ce3e101dac1b53d9f50af94229
Mozart <peterkayk@gmail.com>
2023-03-11 07:16:25 +0300

    Making changes to imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
-import Header from '/Header';
-import AddContact from '/AddContact';
+import Header from './Header';
+import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 
